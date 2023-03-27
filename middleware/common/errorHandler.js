d2d85b4dabac441c1bd1599006ff7240f544e2ab
Muhammad SA <MuhammadSA211999>
2023-03-27 16:19:39 +0600

    error gives html or json response

--- middleware/common/errorHandler.js
+++ middleware/common/errorHandler.js
@@ -2,9 +2,17 @@ const createError = require('http-errors')
 const notFoundHandler = (req, res, next) => {
     next(createError(404, 'Your requested route not found'))
 }
-const errorHandler = (req, res, next) => {
-    res.locals.title = 'Error page'
-    res.render('error.ejs')
+const errorHandler = (err, req, res, next) => {
+    res.locals.error = process.env.NODE_ENV = 'development' ? err : { message: err.message }
+    res.status(err.status || 500)
+    if (res.locals.html) {
+        res.render('error', {
+            title: 'error page'
+        })
+    }
+    else {
+        res.json(res.locals.error)
+    }
 }
 
 module.exports = { notFoundHandler, errorHandler }
